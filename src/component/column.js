8c3843887bc9cdcaf7b6be8e9b9bb7e8d4e92827
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-01-06 16:58:29 +0900

    Change class

--- src/component/column.js
+++ src/component/column.js
@@ -1,14 +1,16 @@
 import Card from "./card.js";
 
 export default function Column({ id, title, tasks }) {
-  return `<div class="column" data-column-id="${id}">
+  return `
+    <div class="column" data-column-id="${id}">
         <div class="column__header">
-            <h4>${title}</h4>
-            <button class="column__header__button--add">
+            <h4 class="column__header--title">${title}</h4>
+            <p class="column__header--count">${tasks.length}</p>
+            <button class="column__header--button--add">
                 +
             </button>
-            <button class="column__header__button--delete">
-                X
+            <button class="column__header--button--delete">
+                ×
             </button>
         </div>
         <div class="column__body">
