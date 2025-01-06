3e3c657aa1fb25220ce923d5b27005badbcaf848
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-01-06 16:58:44 +0900

    Change class and tag

--- src/component/body.js
+++ src/component/body.js
@@ -1,8 +1,8 @@
 import Column from "./column.js";
 
 export default function Body() {
-  const columns = Column({ id: 1, title: "To Do", tasks: [1] }) + Column({ id: 2, title: "In Progress", tasks: [1, 2] }) + Column({ id: 3, title: "Done", tasks: [1, 2, 3] });
-  return `<body class="column">
+  const columns = Column({ id: 1, title: "해야 할 일", tasks: [1] }) + Column({ id: 2, title: "하고 있는 일", tasks: [1, 2] }) + Column({ id: 3, title: "완료한 일", tasks: [1, 2, 3] });
+  return `<div class="body">
         ${columns}
-    </body>`;
+    </div>`;
 }
