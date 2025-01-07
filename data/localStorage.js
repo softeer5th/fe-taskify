df989f675e3286f4f0d0b9e8b0111fb3d634d234
elect10 <wlgnsakswp@naver.com>
2025-01-07 17:00:37 +0900

    chore(storage): make comments

--- data/localStorage.js
+++ data/localStorage.js
@@ -1,3 +1,8 @@
+// value 는 {"title": "~~" ,"body" : " !! " , " author " : " 나야 " , " timestamp" : " 222" }
+// 식의 객체 배열로 저장
+
+// 해당 열에 맞는 값들을 어떻게 불러 올건지 고민후 구현 필요.
+
 export const setStorage = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
   console.log(getStorage(key));
