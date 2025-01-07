52aa1707465e49450b10e81cc6b0dfc77bab76d9
hammsik <asbbrp@gmail.com>
2025-01-07 19:01:08 +0900

    chore: 대략적인 폴더구조 작성 및 테스트

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+import Header from "./components/header.js";
+import Content from "./components/content.js";
+
+const App = () => {
+  const app = document.querySelector("#app");
+  app.appendChild(Header());
+  app.appendChild(Content())
+};
+
+App();
