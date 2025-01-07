e6637e1fff313d3b0a1869549ee8e7fac2b43cd8
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2025-01-07 14:36:36 +0900

    #feat : Task 렌더링 방식 변경 & Task 이동
    * #feat : task rendering method changed directly dom handle to render function
    
    * #feat : Apply move task to other column

--- script/index.js
+++ script/index.js
@@ -1,11 +1,40 @@
-import { addTask } from "./column.js";
+import { createTaskForm, renderTasks } from "./column.js";
 
 const buttonContainers = document.getElementsByClassName('column_button_container');
 
-for(let i=0; i<buttonContainers.length; i++) {
-    const buttons = buttonContainers[i].getElementsByTagName('button');
-    const addButton = buttons[0];
-    addButton.addEventListener('click', ()=>addTask(i));
-    const deleteButton = buttons[1];
+let dragged = null;
+
+export function handleDrag(e, task) {
+    dragged = task;
 }
 
+function handleDrop(e) {
+    const columnIdx = e.target.getAttribute('index');
+    const currentIdx = dragged.column;
+
+    if(columnIdx === currentIdx) return;
+
+    columns[currentIdx] = columns[currentIdx].filter(el => el!=dragged)
+    columns[columnIdx].push({...dragged, column : columnIdx})
+
+    renderTasks(columnIdx);
+    renderTasks(currentIdx);
+}
+
+const columnElements = document.getElementsByClassName('column');
+
+for(let column of columnElements) {
+    column.addEventListener('dragover', (e)=>{e.preventDefault();})
+    column.addEventListener('drop', handleDrop)
+}
+
+for(let i=0; i<buttonContainers.length; i++) {
+    const buttons = buttonContainers[i].getElementsByTagName('button');
+    const [addButton, deleteButton] = buttons;
+    addButton.addEventListener('click', ()=>createTaskForm(i));
+}
+
+export let columns = [];
+for(let i=0; i<3; i++) {
+    columns.push([]);
+}
