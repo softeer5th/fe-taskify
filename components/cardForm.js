ab4c759b536f2a62c98909c4de2f74bb703fb17b
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2025-01-07 16:49:21 +0900

    #feat : Task 개수 카운팅 & Task Form Disable 조절 (#7)
    * #design : 디자인 시스템 & 디자인 적용 (#6)
    
    * #feat : Apply Counting current Task number
    
    * #feat : Apply Disable Task form, when no contents

--- components/cardForm.js
+++ components/cardForm.js
@@ -21,12 +21,35 @@ export function onCancelEdit(task, cardElement) {
     cardElement.innerHTML = taskHTML({ ...task });
 }
 
+export function onChange(e) {
+    const form = e.target.parentNode;
+
+    const title = form.title.value;
+    const content = form.content.value;
+
+    const [cancelButton, deleteButton] = form.getElementsByTagName("button");
+    
+    const currentDisabled = Boolean(deleteButton.disabled);
+    if (title.length !== 0 && content.length !== 0) {
+        if (currentDisabled) {
+            deleteButton.classList.remove("disabled");
+            deleteButton.disabled = false;
+        }
+    } else {
+        if (!currentDisabled) {
+            deleteButton.classList.add("disabled");
+            deleteButton.disabled = true;
+        }
+    }
+}
+
 export function onSubmit(e, columnIdx) {
     // 새로고침 방지
     e.preventDefault();
     const self = e.target;
     const title = self.title.value;
     const content = self.content.value;
+
     self.parentNode.removeChild(self);
 
     const newTask = {
@@ -58,9 +81,9 @@ export default function CardForm() {
                 <input name="content" class="text-default display-medium14" type="text" placeholder="내용을 입력하세요"/>
                 <span>
                     <button class="rounded-100 surface-alt text-default display-bold14" type="button">취소</button>
-                    <button class="rounded-100 surface-brand text-white-default display-bold14" type="submit">제출</button>
+                    <button class="rounded-100 surface-brand text-white-default display-bold14 disabled" disabled type="submit">제출</button>
                 </span>
             </form>
         `
-    );
+        );
 }
--- script/column.js
+++ script/column.js
@@ -1,4 +1,4 @@
-import CardForm, { onSubmit } from '../components/cardForm.js';
+import CardForm, { onChange, onSubmit } from '../components/cardForm.js';
 import { onCancel } from '../components/cardForm.js';
 import { columns } from './index.js';
 import { createTask } from './task.js';
@@ -7,15 +7,19 @@ export function createTaskForm(columnIdx) {
     const cardFormArea = document.getElementsByClassName("card_add")[columnIdx];
     cardFormArea.innerHTML = CardForm();
     const forms = document.getElementsByClassName('card_form');
+    
     for(let form of forms) {
         form.addEventListener('submit', (e)=>onSubmit(e, columnIdx));
+        form.addEventListener('change', (e)=>onChange(e));
         form.getElementsByTagName('button')[0].addEventListener('click', (e)=>onCancel(e))
     }
 }
 
 
 export function renderTasks(columnIdx) {
-    const columnCardList = document.getElementsByClassName('column')[columnIdx].getElementsByTagName('ol')[0];
+    const columnElement = document.getElementsByClassName('column')[columnIdx];
+    const columnCardList = columnElement.getElementsByTagName('ol')[0];
+    const counterElement = columnElement.getElementsByClassName('column_task_counter')[0];
     
     // 현재 카드 전부 제거
     while(columnCardList.firstChild) {
@@ -23,7 +27,8 @@ export function renderTasks(columnIdx) {
     }
 
     const tasks = columns[columnIdx];
-    console.log(tasks);
+    counterElement.textContent = tasks.length;
+    
     for(let task of tasks) {
         const newTask = createTask(task);
         columnCardList.appendChild(newTask);
