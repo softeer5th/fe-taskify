44002e37628cdfcde4f47ba9c4989710285983f9
hamo-o <yheel369@gmail.com>
2025-01-07 17:00:45 +0900

    feat: 함수(컴포넌트)도 처리하도록 수정

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -2,6 +2,7 @@ import { createElement } from "./createElement.js";
 
 const DIRTY_PREFIX = "dirtyindex:";
 const DIRTY_REGEX = /dirtyindex:(\d+):/;
+const DIRTY_SEPERATOR_REGEX_G = /(dirtyindex:\d+:)/g;
 
 /**
  * html을 파싱하여 가상 DOM(VDOM)을 생성합니다.
@@ -17,9 +18,26 @@ export const parser = (strings, ...args) => {
     })
     .join("");
 
-  const domParser = new DOMParser();
-  const doc = domParser.parseFromString(templateString, "text/html");
-  const rootElement = doc.body.firstChild;
+  /**
+   *
+   * @param {string} text - 텍스트 노드
+   * @returns {string | string[]} - 변환된 텍스트 노드
+   */
+  const handleTextNode = (text) => {
+    if (!text.includes(DIRTY_PREFIX)) return text || null;
+
+    const texts = text.split(DIRTY_SEPERATOR_REGEX_G);
+    return texts.map((textPart) => {
+      const dirtyIndex = DIRTY_REGEX.exec(textPart)?.[1];
+      if (!dirtyIndex) return textPart || null;
+
+      const arg = args[Number(dirtyIndex)];
+      if (arg instanceof Array) {
+        return arg;
+      }
+      return arg;
+    });
+  };
 
   /**
    *
@@ -31,29 +49,39 @@ export const parser = (strings, ...args) => {
     const props = {};
     const children = [];
 
+    // 속성 처리
     Array.from(element.attributes || []).forEach((attr) => {
       const { name, value } = attr;
       if (value.includes(DIRTY_PREFIX)) {
         const match = DIRTY_REGEX.exec(value);
         if (match) {
           const arg = args[Number(match[1])];
-          props[name] = arg;
+          props[name] = arg; // DIRTY_PREFIX 처리된 값 치환
+        } else {
+          props[name] = value; // 치환되지 않은 값도 유지
         }
       } else {
-        props[name] = value;
+        props[name] = value; // DIRTY_PREFIX가 없는 일반 속성 처리
       }
     });
 
+    // 자식 노드 처리
     Array.from(element.childNodes || []).forEach((child) => {
       if (child.nodeType === Node.TEXT_NODE) {
-        children.push(child.nodeValue);
+        // 텍스트 노드 처리
+        children.push(...handleTextNode(child.nodeValue));
       } else if (child.nodeType === Node.ELEMENT_NODE) {
+        // 자식 엘리먼트를 재귀적으로 처리
         children.push(parseElement(child));
       }
     });
 
     return createElement(type, props, ...children);
   };
+
+  const domParser = new DOMParser();
+  const doc = domParser.parseFromString(templateString, "text/html");
+  const rootElement = doc.body.firstChild;
 
   return parseElement(rootElement);
 };
