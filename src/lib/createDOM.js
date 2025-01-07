aeb1b5d7648af238d9f86912a59bd1981ba5bc8f
hamo-o <yheel369@gmail.com>
2025-01-07 15:11:24 +0900

    feat: 가상 DOM 만들기, 가상 DOM 실제 DOM 변환

---
+++ src/lib/createDOM.js
@@ -0,0 +1,32 @@
+/**
+ * @typedef VNode
+ * @type {string | number | VDOM | null | undefined}
+ */
+
+/**
+ * @typedef VDOM
+ * @type {object}
+ * @property {string} type - 생성할 HTML 태그의 종류.
+ * @property {{ [key: string]: unknown } | null } props - 생성할 HTML 태그의 속성 객체.
+ * @property {VNode[]} children - 생성할 HTML 태그의 자식 요소.
+ */
+
+/**
+ * @param {VNode} node - 변환할 가상 노드.
+ * @returns {HTMLElement | Text | DocumentFragment} - 변환된 실제 DOM.
+ */
+export const createDOM = (node) => {
+  if (node === null || node === undefined) {
+    return document.createDocumentFragment();
+  }
+
+  if (typeof node === "string" || typeof node === "number") {
+    return document.createTextNode(node);
+  }
+
+  const element = document.createElement(node.type);
+
+  node.children.forEach((child) => element.appendChild(createDOM(child)));
+
+  return element;
+};
