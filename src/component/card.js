b7ef43059c9ef19a202704b0faf358c3d4a4a8f9
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-01-07 10:52:48 +0900

    Update card component

--- src/component/card.js
+++ src/component/card.js
@@ -1,5 +1,14 @@
-export default function Card() {
-  return `<div>
-    Card
-  </div>`;
+export default function Card({ id, title, description, createdAt, device }) {
+  return `
+    <div class="card" data-card-id="${id}">
+        <div class="card__header">
+            <h4 class="card__header--title">${title}</h4>
+            <button class="card__header--delete">×</button>
+        </div>
+        <div class="card__body">
+            <p class="card__body--description">${description}</p>
+            <p class="card__body--createdAt">${createdAt}</p>
+            <p class="card__body--device">${device}</p>
+        </div>
+    </div>`;
 }
