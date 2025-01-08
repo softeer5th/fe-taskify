ddb02d09b0ce070834158b648ab6bd5b7676187e
admin <admin@admins-MacBook-Pro-4.local>
2025-01-08 20:52:51 +0900

    refactor: 이벤트 핸들러 콜백함수 분리

--- js/index.js
+++ js/index.js
@@ -2,35 +2,41 @@ import { todoCardComponent } from "./component/card.js";
 const todoAddButton = document.querySelector(".todo-add-icon");
 const todoTitle = document.querySelector(".todo-title");
 const todoContent = document.querySelector(".todo-content");
-const todoAddBtn = document.querySelector(".todo-add-btn");
+const todoSubmitBtn = document.querySelector(".todo-add-btn");
 const todoFormCard = document.querySelector(".new-card");
 
-todoAddButton.addEventListener("click", () => {
-  todoFormCard.classList.toggle("display-none");
-});
-
 const todoFormInit = () => {
-  todoTitle.value = ""; // form 초기화
-  todoContent.value = ""; // form 초기화
+  // form 초기화
+  todoFormCard.classList.toggle("display-none"); // 입력 폼은 다시 안보이도록.
+  todoTitle.value = "";
+  todoContent.value = "";
 };
 
-todoAddBtn.addEventListener("click", () => {
+const addCard = () => {
   const todoTitleText = todoTitle.value;
   const todoContentText = todoContent.value;
 
-  todoFormCard.classList.toggle("display-none"); // 입력 폼은 다시 안보이도록.
-
   const parser = new DOMParser();
 
-  const todoCardDoc = parser.parseFromString(
+  const todoCardDocument = parser.parseFromString(
     todoCardComponent(todoTitleText, todoContentText),
     "text/html"
   );
-  const todoCardElement = todoCardDoc.body.firstChild;
+
+  const todoCardElement = todoCardDocument.body.firstChild;
 
   // newform 카드 바로 뒤에 추가
   todoFormCard.after(todoCardElement);
-  todoFormInit();
-});
+
+  todoFormInit(); // 입력했던 값을 다시 빈 문자열로 초기화.
+};
+
+const showCardForm = () => {
+  todoFormCard.classList.toggle("display-none");
+};
+
+todoAddButton.addEventListener("click", showCardForm);
+
+todoSubmitBtn.addEventListener("click", addCard);
 
 // next > new 제목과 내용 입력이 다 되면 등록 버튼 활성화. 이전에는 disabled.
