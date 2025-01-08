0aff706a30c66454a42a73e7a670e9c7cbc6f6f9
hamo-o <yheel369@gmail.com>
2025-01-08 12:30:14 +0900

    fix: 중복 코드 삭제

--- src/lib/jsx-runtime/createDOM.js
+++ src/lib/jsx-runtime/createDOM.js
@@ -1,17 +1,4 @@
 /**
- * @typedef VNode
- * @type {string | number | VDOM | null | undefined}
- */
-
-/**
- * @typedef VDOM
- * @type {object}
- * @property {string} type - 생성할 HTML 태그의 종류.
- * @property {{ [key: string]: unknown } | null } props - 생성할 HTML 태그의 속성 객체.
- * @property {VNode[]} children - 생성할 HTML 태그의 자식 요소.
- */
-
-/**
  * @param {VNode} node - 변환할 가상 노드.
  * @returns {HTMLElement | Text | DocumentFragment} - 변환된 실제 DOM.
  */
