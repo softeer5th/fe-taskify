fff1f722d3af7eda89c92769a26a250d079e79a3
hammsik <asbbrp@gmail.com>
2025-01-08 15:24:43 +0900

    style: column 및 card 레이아웃 템플릿 작성 및 테스트

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,21 @@
-import Header from "./components/header.js";
-import Content from "./components/content.js";
+import Header from './components/header.js';
+import Content from './components/content.js';
 
 const App = () => {
-  // const app = document.querySelector("#app");
-  // app.appendChild(Header());
-  // app.appendChild(Content())
+  // 더미 컬럼
+  const columnClone = document
+    .getElementById('column-template')
+    .content.cloneNode(true);
+
+  // 더미 카드컴포넌트
+  const cardTemplate = document.getElementById('card-template');
+  for (let i = 0; i < 3; i++) {
+    const cardClone = cardTemplate.content.cloneNode(true);
+    columnClone.getElementById('column-section').appendChild(cardClone);
+  }
+
+  // 더미 컬럼을 추가
+  document.getElementById('main').appendChild(columnClone);
 };
 
 App();
