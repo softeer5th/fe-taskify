0b03954f1124c2a4815181677d25003bf2ab036e
dgfh0450 <dgfh0450@gmail.com>
2025-01-08 11:18:17 +0900

    #feat : create Task Form input add maxlength 500 / apply word break to task component

--- components/cardForm.js
+++ components/cardForm.js
@@ -77,8 +77,8 @@ export default function CardForm() {
     return (
         `
             <form class="card_form card surface-default shadow-normal rounded-100">
-                <input name="title" class="text-strong display-bold14" type="text" placeholder="제목을 입력하세요"/>
-                <input name="content" class="text-default display-medium14" type="text" placeholder="내용을 입력하세요"/>
+                <input name="title" class="text-strong display-bold14" type="text" placeholder="제목을 입력하세요" maxlength="500" />
+                <input name="content" class="text-default display-medium14" type="text" placeholder="내용을 입력하세요" maxlength="500" />
                 <span>
                     <button class="rounded-100 surface-alt text-default display-bold14" type="button">취소</button>
                     <button class="rounded-100 surface-brand text-white-default display-bold14 disabled" disabled type="submit">제출</button>
