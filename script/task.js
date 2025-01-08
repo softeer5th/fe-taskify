e19bf205af522f1529dac80e107af9a85b9e931e
dgfh0450 <dgfh0450@gmail.com>
2025-01-08 21:52:29 +0900

    #feat : task move column to column

--- script/task.js
+++ script/task.js
@@ -1,4 +1,4 @@
-import CardForm, { onCancelEdit, onEdit, onSubmit } from "../components/cardForm.js";
+import CardForm, { onCancelEdit, onEdit } from "../components/cardForm.js";
 import Modal from "../components/modal.js";
 import { renderTasks } from "./column.js";
 import { columns, handleDrag } from "./index.js";
@@ -6,22 +6,22 @@ import { createModalChildren } from "./modal.js";
 
 export function createDeleteModal(task) {
     const body = document.getElementsByTagName("body")[0];
-    const modalElement = createModalChildren('선택한 카드를 삭제할까요?', ()=>deleteTask(task))
+    const modalElement = createModalChildren("선택한 카드를 삭제할까요?", () =>
+        deleteTask(task)
+    );
     const modal = Modal(modalElement);
     body.appendChild(modal);
 }
 
-
-
 export function createTask(task) {
-    const {title, content, created, column} = task;
-    
+    const { title, content, created, column } = task;
+
     // 새 카드 컴포넌트 생성
-    const newCard = document.createElement('li')
-    newCard.setAttribute('class', 'card surface-default shadow-normal rounded-100')
-    newCard.setAttribute('draggable', 'true')
-    newCard.innerHTML = taskHTML({title, content});
-    newCard.addEventListener("dragstart", (e)=>handleDrag(e, task));
+    const newCard = document.createElement("li");
+    newCard.setAttribute("class", "card surface-default shadow-normal rounded-100");
+    newCard.setAttribute("draggable", "true");
+    newCard.innerHTML = taskHTML({ title, content });
+    newCard.addEventListener("dragstart", (e) => handleDrag(e, task));
     // 이벤트 등록
     taskEventHandler(newCard, task);
 
@@ -29,45 +29,47 @@ export function createTask(task) {
 }
 
 export function deleteTask(task) {
-    const {title, content, created, column} = task;
-    columns[column] = columns[column].filter(el => el != task);
+    const { title, content, created, column } = task;
+    columns[column] = columns[column].filter((el) => el != task);
     renderTasks(column);
 }
 
 export function editTask(cardElement, task) {
-    const {title, content, created, column} = task;
+    const { title, content, created, column } = task;
     cardElement.innerHTML = CardForm();
-    const inputs = cardElement.getElementsByTagName('input');
-    const [titleInput, contentInput] = inputs
+    const inputs = cardElement.getElementsByTagName("input");
+    const [titleInput, contentInput] = inputs;
     titleInput.value = title;
     contentInput.value = content;
-    const form = cardElement.getElementsByTagName('form')[0];
-    form.addEventListener('submit', (e)=>{onEdit(e, task)})
-    form.getElementsByTagName('button')[0].addEventListener('click', ()=>onCancelEdit(task, cardElement))
+    const form = cardElement.getElementsByTagName("form")[0];
+    form.addEventListener("submit", (e) => {
+        onEdit(e, task);
+    });
+    form.getElementsByTagName("button")[0].addEventListener("click", () =>
+        onCancelEdit(task, cardElement)
+    );
 }
 
-export function taskHTML({title, content}) {
-    return (
-    `
+export function taskHTML({ title, content }) {
+    return `
         <div class="card_text_container">
             <h4 class="text-strong display-bold14">${title}</h4>
             <p class="text-weak display-medium14">${content}</p>
         </div>
         <div class="card_button_container">
             <button>
-                <img width="24" height="24" src="/public/icon/closed.svg"/>
+                <img draggable="false" width="24" height="24" src="/public/icon/closed.svg"/>
             </button>
             <button>
-                <img width="24" height="24" src="/public/icon/edit.svg"/>
+                <img draggable="false" width="24" height="24" src="/public/icon/edit.svg"/>
             </button>
         </div>
-    `
-    )
+    `;
 }
 
 export function taskEventHandler(cardElement, task) {
-    const buttons = cardElement.getElementsByTagName('button');
+    const buttons = cardElement.getElementsByTagName("button");
     const [deleteButton, editButton] = buttons;
-    deleteButton.addEventListener('click', ()=>createDeleteModal(task));
-    editButton.addEventListener('click', ()=>editTask(cardElement, task));
+    deleteButton.addEventListener("click", () => createDeleteModal(task));
+    editButton.addEventListener("click", () => editTask(cardElement, task));
 }
