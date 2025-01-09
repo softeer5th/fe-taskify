4fec683be00166afab4ee480a8caf9e970b753d7
hamo-o <yheel369@gmail.com>
2025-01-09 19:52:30 +0900

    feat: 버튼 종류에 따른 스타일링 추가

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -8,15 +8,18 @@ import { Text } from "../Text/index.js";
  * @param {object} props - 컴포넌트의 props
  * @param {string} [props.label] - 버튼에 표시할 라벨
  * @param {"contained"|"ghost"} [props.type] - 버튼의 타입
+ * @param {"sub"|"primary"|"danger"} [props.variant] - 버튼의 종류
  * @param {boolean} [props.showIcon] - 아이콘을 표시할지 여부
  * @param {Function} [props.onClick] - 클릭 이벤트 시 호출할 함수
  * @param {boolean} [props.isFull] - 버튼의 너비를 꽉 채울지 여부
+ * @param props.state
  * @returns {HTMLElement} - 생성된 버튼 가상돔
  */
 export const Button = ({
   showIcon = false,
   label,
   type = "contained",
+  variant = "primary",
   isFull = false,
   onClick,
 }) => {
@@ -24,9 +27,9 @@ export const Button = ({
    * @returns {string} - 버튼의 클래스명
    */
   const findClasses = () => {
-    const buttonType = `button-${type}`;
-    if (isFull) return `${buttonType} button-full`;
-    return buttonType;
+    const buttonClass = `button-${type} button-${variant}`;
+    if (isFull) return `${buttonClass} button-full`;
+    return buttonClass;
   };
 
   return parser`
