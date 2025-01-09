a2714cee43dbec3d321660f4e5856c4b24d6a58b
young-jin-son <sdudwls128@naver.com>
2025-01-09 21:58:23 +0900

    refactor: Button 컴포넌트에 동적 스타일 클래스 추가

--- js/components/Button/Button.js
+++ js/components/Button/Button.js
@@ -1,29 +1,36 @@
-export default function Button ({
-    type = 'button', 
-    size = 'medium', 
-    format = 'default',  // [icon, text, icon-text]
-    style = 'default',
-    disabled = false,
-    onClick,
-    children,            
+export default function Button({
+  type = "button",
+  size = "medium",
+  format = "default", // [icon, text, icon-text]
+  style = "default", // [default, brand, danger]
+  disabled = false,
+  onClick,
+  children,
 }) {
-    const button = document.createElement('button');
-    button.setAttribute('type', type);
-    
-    button.classList.add('button', `button__${size}`, `button__${format}`, `button--${style}`);
+  const button = document.createElement("button");
+  button.setAttribute("type", type);
 
-    if (disabled) {
-        button.setAttribute('disabled', 'true');
-        button.classList.add('button--disabled');
-    }
+  button.classList.add(
+    "button",
+    `button__${size}`,
+    `button__${format}`,
+    `surface-${style === "default" ? "alt" : style}`,
+    `text-${style === "default" ? "" : "white-"}default`,
+    `display-bold14`
+  );
 
-    if (children) {
-        button.appendChild(children);
-    }
+  if (disabled) {
+    button.setAttribute("disabled", "true");
+    button.classList.add("button--disabled");
+  }
 
-    if (onClick) {
-        button.addEventListener('click', onClick);
-    }
+  if (children) {
+    button.appendChild(children);
+  }
 
-    return button;
+  if (onClick) {
+    button.addEventListener("click", onClick);
+  }
+
+  return button;
 }
