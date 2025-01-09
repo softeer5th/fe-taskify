689a3577def304014b30d160eb4194fd921e9057
young-jin-son <sdudwls128@naver.com>
2025-01-09 22:04:30 +0900

    feat: TaskCard 컴포넌트 생성

--- js/components/TaskCard/TaskCard.js
+++ js/components/TaskCard/TaskCard.js
@@ -1,21 +1,53 @@
-export default function TaskCard() {
+import Button from "../Button/Button.js";
+import createImg from "../../utils/createImg.js";
+
+export default function TaskCard({id = 1}) {
+    const deleteBtnClickHandler = () => {
+        console.log('delete button clicked');
+        taskCard.remove();
+    }
+
+    const editBtnClickHandler = () => {
+        console.log('edit button clicked');
+    }
     const taskCard = document.createElement('div');
     taskCard.className = 'task-card';
 
     const taskCardDetails = document.createElement('div');
     taskCardDetails.classList.add('task-card__details');
 
-    const newTaskTitle = document.createElement('div');
-    newTaskTitle.classList = 'task-card__title';
-    newTaskTitle.innerText = 'title'
+    const taskCardTitle = document.createElement('div');
+    taskCardTitle.classList.add('task-card__title', 'display-bold14', 'text-strong');
+    taskCardTitle.innerText = `title ${id}`;
     
-    const newTaskContent = document.createElement('div');
-    newTaskContent.className = 'task-card__content';
-    newTaskContent.innerText = 'content'
-    
-    taskCardDetails.append(newTaskTitle, newTaskContent);
+    const TaskCardContent = document.createElement('div');
+    TaskCardContent.classList.add('task-card__content', 'display-medium14', 'text-default');
+    TaskCardContent.innerText = `content ${id}`
 
-    taskCard.append(taskCardDetails)
+    const taskCardButtons = document.createElement('div');
+    taskCardButtons.classList.add('task-card__buttons');
+
+    const deleteButton = Button({
+        format: 'icon',
+        onClick: deleteBtnClickHandler,
+        children: createImg({
+            src: '/assets/icons/closed.svg',
+            alt: 'delete button' 
+        }),
+    });
+
+    const editButton = Button({
+        format: 'icon',
+        onClick: editBtnClickHandler,
+        children: createImg({
+            src: '/assets/icons/edit.svg',
+            alt: 'edit button'
+        }),
+    });
+
+    taskCardDetails.append(taskCardTitle, TaskCardContent);
+    taskCardButtons.append(deleteButton, editButton);
+    taskCard.append(taskCardDetails, taskCardButtons);
     
     return taskCard;
 }
