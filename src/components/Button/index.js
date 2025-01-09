eb243d0b92efc190c29de01703fafcd4c8d71cb9
hamo-o <yheel369@gmail.com>
2025-01-09 17:57:18 +0900

    feat: 버튼을 꽉차게 하는 옵션 추가

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -10,18 +10,27 @@ import { Text } from "../Text/index.js";
  * @param {"contained"|"ghost"} [props.type] - 버튼의 타입
  * @param {boolean} [props.showIcon] - 아이콘을 표시할지 여부
  * @param {Function} [props.onClick] - 클릭 이벤트 시 호출할 함수
+ * @param {boolean} [props.isFull] - 버튼의 너비를 꽉 채울지 여부
  * @returns {HTMLElement} - 생성된 버튼 가상돔
  */
 export const Button = ({
   showIcon = false,
   label,
   type = "contained",
+  isFull = false,
   onClick,
 }) => {
-  const buttonType = `button-${type}`;
+  /**
+   * @returns {string} - 버튼의 클래스명
+   */
+  const findClasses = () => {
+    const buttonType = `button-${type}`;
+    if (isFull) return `${buttonType} button-full`;
+    return buttonType;
+  };
 
   return parser`
-<button onClick=${onClick} class="${buttonType}">
+<button onClick=${onClick} class="${findClasses()}">
     ${showIcon && Icon({ name: "plus", size: "md", fillColor: colors.text.white.default })}
     ${label && Text({ text: label, typo: typos.selected.bold[16] })}
 </button>`;
