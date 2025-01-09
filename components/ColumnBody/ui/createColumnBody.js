34ae3a2735ae6e35ec4e1e8139ac90736b0a0e7e
rbgksqkr <rbgks1937@naver.com>
2025-01-09 23:04:20 +0900

    refactor: 드래그 앤 드롭 DOM 반영 함수 개선

--- components/ColumnBody/ui/createColumnBody.js
+++ components/ColumnBody/ui/createColumnBody.js
@@ -39,6 +39,7 @@ const createColumnBody = ({ sectionId, items }) => {
     e.preventDefault();
     const draggedElement = document.querySelector(".dragging");
 
+    // shadowElement 앞에 draggedElement 삽입 & shadowElement 제거
     if (shadowElement && draggedElement) {
       $columnBody.insertBefore(draggedElement, shadowElement);
       shadowElement.remove();
@@ -46,12 +47,13 @@ const createColumnBody = ({ sectionId, items }) => {
     }
 
     const newSectionId = $columnBody.closest(".column__container").id;
-    const itemId = draggedElement.dataset.id;
+    const itemId = Number(draggedElement.dataset.id);
 
     updateTodoList(newSectionId, itemId);
   };
 
   const handleDragLeave = (e) => {
+    // 칼럼 벗어남 & 잔상 존재
     if (!e.currentTarget.contains(e.relatedTarget) && shadowElement) {
       shadowElement.remove();
       shadowElement = null;
@@ -72,20 +74,21 @@ const createColumnBody = ({ sectionId, items }) => {
 
 export default createColumnBody;
 
+// drag drop 했을 때 DOM 업데이트
+// 어떤 section의 어떤 아이템인지 특정
 const updateTodoList = (newSectionId, itemId) => {
   const todoList = loadLocalStorage();
+  const section = todoList.find((section) => section.id === newSectionId);
+  const draggedItem = section.items.find((item) => item.id === itemId);
 
-  let draggedItem = null;
-  const updatedList = todoList.map((section) => {
-    if (section.items.find((item) => item.id == itemId)) {
-      draggedItem = section.items.find((item) => item.id == itemId);
-      return {
-        ...section,
-        items: section.items.filter((item) => item.id != itemId),
-      };
-    }
-    return section;
-  });
+  const updatedList = todoList.map((section) =>
+    draggedItem
+      ? {
+          ...section,
+          items: section.items.filter((item) => item.id !== itemId),
+        }
+      : section
+  );
 
   const finalList = updatedList.map((section) => {
     if (section.id === newSectionId && draggedItem) {
