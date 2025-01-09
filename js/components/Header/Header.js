4faaf5705a7e53588f744e64b48db605ee0992fc
young-jin-son <sdudwls128@naver.com>
2025-01-09 22:02:19 +0900

    chore: 헤더의 히스토리 버튼 생성 로직 간소화

--- js/components/Header/Header.js
+++ js/components/Header/Header.js
@@ -2,31 +2,27 @@ import Button from "../Button/Button.js";
 import createImg from "../../utils/createImg.js";
 
 export default function Header() {
-    const header = document.createElement('header');
-    header.classList.add('header');
+  const header = document.createElement("header");
+  header.classList.add("header");
 
-    const headerTitle = document.createElement('div');
-    headerTitle.classList.add('header__title', 'display-bold24');
-    headerTitle.textContent = 'TASKFY';
-    
-    const historyClickHandler = () => {
-        console.log('History button clicked!');
-    };
-   
-    const historyButton = Button({
-        type: 'button',
-        size: 'midium',
-        format: 'icon',
-        style: 'default',
-        disabled: false,
-        onClick: historyClickHandler,
-        children: createImg({
-            src: '/assets/icons/clock.svg',
-            alt: 'history icon'
-        }),
-    });
+  const headerTitle = document.createElement("div");
+  headerTitle.classList.add("header__title", "display-bold24");
+  headerTitle.textContent = "TASKFY";
 
-    header.append(headerTitle, historyButton);
+  const historyClickHandler = () => {
+    console.log("History button clicked!");
+  };
 
-    return header;
+  const historyButton = Button({
+    format: "icon",
+    onClick: historyClickHandler,
+    children: createImg({
+      src: "/assets/icons/clock.svg",
+      alt: "history icon",
+    }),
+  });
+
+  header.append(headerTitle, historyButton);
+
+  return header;
 }
