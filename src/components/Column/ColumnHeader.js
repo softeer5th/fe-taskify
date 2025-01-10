01a3749f37bb4f185a84844418f01d0666cd9bef
kimdongyoung <wwweric12@naver.com>
2025-01-10 10:46:32 +0900

    feat: column-title UI 구현

---
+++ src/components/Column/ColumnHeader.js
@@ -0,0 +1,40 @@
+import { loadCss } from '../../utils/loadcss.js';
+import { Badge } from '../Badge/Badge.js';
+import { Button } from '../Button/Button.js';
+
+export function ColumnHeader(title,badgeContent,plusEvent,closedEvent){
+    const columnHeader = document.createElement('div');
+    columnHeader.className = "column-header";
+    columnHeader.innerHTML = `
+    <div class="column-header-container">
+        <div class="column-title-container">
+          <div class="column-header-title">${title}</div>
+        </div>
+    </div>
+    `;
+    
+
+    const plusButton = Button({type:'icon',icon:'plus',backgroundColor:'grayscale100',textColor:"grayscale500"});
+    const closedButton = Button({type:'icon',icon:'close',backgroundColor:'grayscale100',textColor:'grayscale500'});
+
+    const columnTitle = columnHeader.querySelector('.column-header-title');
+    const titleContainer = columnHeader.querySelector('.column-title-container');
+    const badge = Badge(badgeContent);
+    
+    columnTitle.insertAdjacentElement('afterend', badge);
+    titleContainer.insertAdjacentElement('afterend',closedButton);
+    titleContainer.insertAdjacentElement('afterend', plusButton);
+    
+
+
+
+    plusButton.addEventListener('click', plusEvent);
+  
+    closedButton.addEventListener('click', closedEvent);
+
+
+    loadCss('../src/components/Column/ColumnHeader.css')
+    return columnHeader;
+}
+
+
