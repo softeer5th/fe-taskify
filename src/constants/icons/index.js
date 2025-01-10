f0c36c48d075d6bbd79768c43cde30ab62d89c6b
hamo-o <yheel369@gmail.com>
2025-01-10 11:30:05 +0900

    fix: 아이콘에 onClick 이벤트 달기

--- src/constants/icons/index.js
+++ src/constants/icons/index.js
@@ -9,16 +9,21 @@ import icons from "./icons.js";
  * @param {"md"|"lg"} [props.size] - 아이콘 크기
  * @param {string}[props.fillColor] - 아이콘 채우기 색상
  * @param {string} [props.strokeColor] - 아이콘 테두리 색상
+ * @param {Function} [props.onClick] - 클릭 이벤트 시 호출할 함수
  * @returns {VDOM} - 생성된 아이콘 엘리먼트
  */
 export const Icon = ({
-  name, size = "lg", fillColor = "", strokeColor = "",
+  name, size = "lg", fillColor = "", strokeColor = "", onClick,
 }) => {
   const SIZE_NUM = size === "lg" ? 24 : 16;
   const element = icons[name];
 
   if (fillColor) element.props.fill = fillColor;
   if (strokeColor) element.props.stroke = strokeColor;
+  if (typeof onClick === "function") {
+    element.props.cursor = "pointer";
+    element.events.click = onClick;
+  }
   element.props.width = SIZE_NUM;
   element.props.height = SIZE_NUM;
 
