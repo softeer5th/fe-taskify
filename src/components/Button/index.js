5196e78d65972ad53da7ed2505d4aee6a8d269a1
hamo-o <yheel369@gmail.com>
2025-01-10 11:51:53 +0900

    design: 버튼 폰트 변경

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -35,8 +35,7 @@ export const Button = ({
 
   return parser`
     <button onClick=${onClick} class="${findClasses()}">
-    ${showIcon && Icon({ name: "plus", size: "md", fillColor: colors.text.white.default })
-}
+    ${showIcon && Icon({ name: "plus", size: "md", fillColor: colors.text.white.default })}
     ${label && Text({ text: label, typo: typos.selected.bold[16] })}
     </button>`;
 };
