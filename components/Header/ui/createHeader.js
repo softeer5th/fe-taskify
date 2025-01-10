3498710d46dc6e1f51541d419160fa077a8a6f5b
rbgksqkr <rbgks1937@naver.com>
2025-01-10 11:40:01 +0900

    feat: 로컬 스토리지 데이터를 기반으로 정렬 후 UI 업데이트

--- components/Header/ui/createHeader.js
+++ components/Header/ui/createHeader.js
@@ -1,5 +1,7 @@
 import { IMAGE } from "../../../assets/index.js";
 import { createButton, createElement, createImg } from "../../../dom.js";
+import { loadLocalStorage } from "../../../utils/localStorage.js";
+import ColumnSection from "../../ColumnSection/ColumnSection.js";
 
 const toggleText = {
   "최신 순": "생성 순",
@@ -19,6 +21,39 @@ const createHeader = () => {
   // 2. 가져온 데이터를 정렬 기준에 맞게 정렬한다. (생성 순, 최신 순)
   // 3. 데이터를 기준으로 column__section을 갈아끼운다.
   const handleClickSort = (e) => {
+    // 1. 로컬 스토리지에서 데이터를 불러온다.
+    const todoList = loadLocalStorage();
+
+    // 2. 토글된 정렬 기준으로 데이터를 정렬한다.
+    const sortType = toggleText[e.currentTarget.textContent];
+
+    const sortedTodoList = [...todoList].map((section) => {
+      section.items.sort((a, b) => {
+        if (sortType === "최신 순") {
+          return (
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+          );
+        } else if (sortType === "생성 순") {
+          return (
+            new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+          );
+        }
+        return 0;
+      });
+
+      return section;
+    });
+
+    // 3. 데이터를 기준으로 column__section을 갈아끼운다.
+    const $main = document.querySelector("main.column__section");
+
+    const $fragment = document.createDocumentFragment();
+    sortedTodoList.forEach((todo) => {
+      $fragment.appendChild(ColumnSection(todo));
+    });
+
+    $main.replaceChildren($fragment);
+
     // 텍스트 토글 (생성 순 / 최신 순)
     e.currentTarget.querySelector("span").textContent =
       toggleText[e.currentTarget.textContent];
