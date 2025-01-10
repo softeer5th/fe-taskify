0b27b2a38d3b0eadb5bc774a9b4169b88d232236
rbgksqkr <rbgks1937@naver.com>
2025-01-10 22:39:23 +0900

    refactor: Header 내의 LeftHeader, HistoryButton 분리

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,17 @@
+import { createElement } from "../../dom.js";
 import loadStyleSheet from "../../utils/loadStyleSheet.js";
-import createHeader from "./ui/createHeader.js";
+import createHistoryButton from "./ui/createHistoryButton.js";
+import createLeftHeader from "./ui/createLeftHeader.js";
 
 loadStyleSheet("/components/Header/styles.css");
 
 const Header = () => {
-  return createHeader();
+  const $header = createElement("header", { className: "header" });
+  const $leftHeader = createLeftHeader();
+  const $historyButton = createHistoryButton();
+
+  $header.append($leftHeader, $historyButton);
+  return $header;
 };
 
 export default Header;
