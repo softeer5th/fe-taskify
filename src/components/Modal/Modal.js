54ab11d1d6eb05b77f4e716c22dcc37d66211899
kimdongyoung <wwweric12@naver.com>
2025-01-12 22:13:38 +0900

    feat: Modal UI 구현

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,46 @@
+import { loadCss } from '../../utils/loadcss.js';
+import { Button } from '../Button/Button.js';
+
+export function Modal({content,buttonProps}){
+    const modal = document.createElement('div');
+    modal.className = 'modal-container';
+    modal.innerHTML =`
+    <div class='modal-content'>${content} </div>
+    `
+    
+    const buttonBox = document.createElement('div');
+    buttonBox.className = 'card-button-box';
+
+    // 버튼 인자값을 받아 올거임 
+    
+    const leftButton = Button({
+        type:'text',
+        text:'button',
+        backgroundColor: 'grayscale100',
+        textColor:'grayscale600',
+        eventFuntion:()=>{}
+    })
+
+    const rightButton = Button({
+        type:'text',
+        text:'button',
+        backgroundColor: 'accentRed',
+        textColor:'grayscale50',
+        eventFuntion:()=>{}
+    })
+    // 여기까지 
+    
+    buttonBox.insertAdjacentElement('beforeend',leftButton);
+    buttonBox.insertAdjacentElement('beforeend',rightButton);
+
+    modal.insertAdjacentElement('beforeend', buttonBox);            
+    
+
+
+    
+
+    loadCss('../src/components/Modal/Modal.css')
+    return modal;
+}
+
+
