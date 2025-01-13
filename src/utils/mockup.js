5d0b1291ecf15d0a322da8e4837991fd0049ac24
kimdongyoung <wwweric12@naver.com>
2025-01-13 23:13:45 +0900

    feat: mockup data 생성

---
+++ src/utils/mockup.js
@@ -0,0 +1,63 @@
+export const data ={
+    "todos": [
+      {
+        "id": 1,
+        "title": "장보기",
+        "content": "우유, 계란, 빵 구매",
+        "dueDate": "2023-12-10",
+        "author": "author by web"
+      },
+      {
+        "id": 2,
+        "title": "보고서 작성",
+        "content": "월별 매출 보고서",
+        "dueDate": "2023-12-15",
+        "author": "author by web"
+      }
+    ],
+    "progress": [
+        {
+          "id": 1,
+          "title": "장보기",
+          "content": "우유, 계란, 빵 구매",
+          "dueDate": "2023-12-10",
+          "author": "author by web"
+        },
+        {
+          "id": 2,
+          "title": "보고서 작성",
+          "content": "월별 매출 보고서",
+          "dueDate": "2023-12-15",
+          "author": "author by web"
+        },
+        {
+            "id": 2,
+            "title": "보고서 작성",
+            "content": "월별 매출 보고서",
+            "dueDate": "2023-12-15",
+            "author": "author by web"
+          }
+      ],
+      "done": [
+        {
+          "id": 1,
+          "title": "장보기",
+          "content": "우유, 계란, 빵 구매",
+          "dueDate": "2023-12-10",
+          "author": "author by web"
+          
+        },
+        {
+          "id": 2,
+          "title": "보고서 작성",
+          "content": "월별 매출 보고서",
+          "dueDate": "2023-12-15",
+          "author": "author by web"
+          
+        }
+      ]
+  }
+  
+  
+  
+  
