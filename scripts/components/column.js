d4e2c592afc45bd5e21c7a1fe003aa6397e5f5ee
hammsik <asbbrp@gmail.com>
2025-01-13 18:32:59 +0900

    [TASKIFY-10] feat: 컬럼 데이터 임시 생성

--- scripts/components/column.js
+++ scripts/components/column.js
@@ -1,52 +1,32 @@
 import Card from './card.js';
+/**
+ * @typedef {Object} Card
+ * @property {number} id - 카드 ID
+ * @property {string} title - 카드 제목
+ * @property {string} body - 카드 내용
+ * @property {string} createdDate - 카드 생성일
+ */
 
-const Column = (columnTitle) => {
+/**
+ * @typedef {Object} Column
+ * @property {number} columnId - 컬럼 ID
+ * @property {string} columnName - 컬럼 이름
+ * @property {Card[]} cards - 카드 배열
+ */
+
+/** 
+ * 컬럼 컴포넌트
+ * @param {Column} columnData - 컬럼 데이터
+ */
+const Column = (columnData) => {
   const column = document
     .getElementById('column-template')
     .content.cloneNode(true);
 
-  column.querySelector('h2').textContent = columnTitle;
-
-  column.querySelector('ul').appendChild(
-    Card('default', {
-      id: 1,
-      title: '모던 자바스크립트 예제 실습',
-      body: '1장 예제 내용 실습 후, 커밋까지',
-      createdDate: '2021-08-10',
-    })
-  );
-  column.querySelector('ul').appendChild(
-    Card('add', {
-      id: 2,
-      title: 'GitHub 공부하기',
-      body: 'add, commit, push',
-      createdDate: '2021-08-10',
-    })
-  );
-  column.querySelector('ul').appendChild(
-    Card('drag', {
-      id: 3,
-      title: '카드 제목',
-      body: '카드 내용',
-      createdDate: '2021-08-10',
-    })
-  );
-  column.querySelector('ul').appendChild(
-    Card('place', {
-      id: 4,
-      title: '카드 제목',
-      body: '카드 내용',
-      createdDate: '2021-08-10',
-    })
-  );
-  column.querySelector('ul').appendChild(
-    Card('add', {
-      id: 4,
-      title: null,
-      body: null,
-      createdDate: '2021-08-10',
-    })
-  );
+  column.querySelector('h2').textContent = columnData.columnName;
+  columnData.cards.forEach((cardData) => {
+    column.querySelector('ul').appendChild(Card('default', cardData));
+  })
 
   column.querySelector('#add-card').addEventListener('click', (e) => {
     console.log('add card');
