7c4ca5f841df368fa418ec71c666e104f0d9ef9a
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2025-01-13 18:50:18 +0900

    #refactor : Header, Log 컴포넌트 모듈화, Column 정렬, 카운터 적용 및 Column 재렌더링 최적화 (#14)
    * #refactor : Column, Task, Modal Component Modulazation Refactoring
    
    * #refactor : Modularization Header Component & Log Component
    
    * #refactor : refactoring render column function & apply sort column
    
    * #refactor : refactoring task counter & compare tasks by taskid
    
    * #fix : Log Component Rendering typo error bug fix
    
    * #fix : change delete log modal message

--- components/log.js
+++ components/log.js
@@ -1,7 +1,8 @@
 import { timeCalculator } from "../script/utils.js";
 
-export default function LogLayerHTML() {
-    return `
+export function LogComponent(columns) {
+    function template() {
+        return `
             <div id="log_layer_header">
                 <p class="text-strong display-bold16">사용자 활동 기록</p>
                 <button>
@@ -14,66 +15,142 @@ export default function LogLayerHTML() {
             <ol id="log_layer_list">
             </ol>
         `;
-}
-
-export function EmptyLogHTML() {
-    return `
-        <p class="text-weak display-medium14">사용자 활동 기록이 없습니다.</p>
-    `;
-}
-
-export function LogHTML(log) {
-    const { task, type: actionType, updated, destination } = log;
-    const { title, content, column, created } = task;
-    const { time, type: timeType } = timeCalculator(updated, new Date());
-
-    function addTemplate({ title, column }) {
-        return `<span>${title}</span>을(를) <span>${column}</span>에서 <span>등록</span>하였습니다.`;
     }
 
-    function removeTemplate({ title, column }) {
-        return `<span>${title}</span>을(를) <span>${column}</span>에서 <span>삭제</span>하였습니다.`;
-    }
-
-    function updateTemplate({ title }) {
-        return `<span>${title}</span>을(를) <span>변경</span>하였습니다.`;
-    }
-
-    function moveTemplate({ title, column, destination }) {
-        return `<span>${title}</span>을(를) <span>${column}</span>에서 <span>${destination}</span>으로 <span>이동</span>하였습니다.`;
-    }
-
-    let template = ``;
-
-    switch (actionType) {
-        case "add":
-            template = addTemplate({ title, column });
-            break;
-        case "remove":
-            template = removeTemplate({ title, column });
-            break;
-        case "update":
-            template = updateTemplate({ title });
-            break;
-        case "move":
-            template = moveTemplate({ title, column, destination });
-    }
-
-    return `
-            <div>
-                <div class="log_profile border-default surface-alt">
-                </div>
-            </div>
-            <div>
-                <p class="text-default display-medium14">나</p>
-                <p class="text-default display-medium14">${template}</p>
-                <p class="text-weak display-medium12">${time}${timeType} 전</p>
-            </div>
-    `;
-}
-
-export function DeleteLogHTML() {
-    return `
-            <button class="display-bold14 text-danger">기록 전체 삭제</button>
+    function templateEmpty() {
+        return `
+            <p class="text-weak display-medium14">사용자 활동 기록이 없습니다.</p>
         `;
+    }
+
+    function templateLog(log) {
+        const { task, type: actionType, updated, destination } = log;
+        const { title, content, column, created } = task;
+        const { time, type: timeType } = timeCalculator(updated, new Date());
+
+        const originTitle = findColumnName(column);
+    
+        function templateAdd({ title, originTitle }) {
+            return `<span>${title}</span>을(를) <span>${originTitle}</span>에서 <span>등록</span>하였습니다.`;
+        }
+    
+        function templateRemove({ title, originTitle }) {
+            return `<span>${title}</span>을(를) <span>${originTitle}</span>에서 <span>삭제</span>하였습니다.`;
+        }
+    
+        function templateUpdate({ title }) {
+            return `<span>${title}</span>을(를) <span>변경</span>하였습니다.`;
+        }
+    
+        function templateMove({ title, originTitle, destinationTitle }) {
+            return `<span>${title}</span>을(를) <span>${originTitle}</span>에서 <span>${destinationTitle}</span>으로 <span>이동</span>하였습니다.`;
+        }
+    
+        let template = ``;
+    
+        switch (actionType) {
+            case "ADD":
+                template = templateAdd({ title, originTitle });
+                break;
+            case "REMOVE":
+                template = templateRemove({ title, originTitle });
+                break;
+            case "UPDATE":
+                template = templateUpdate({ title });
+                break;
+            case "MOVE":
+                const destinationTitle = findColumnName(destination);
+                template = templateMove({ title, originTitle, destinationTitle });
+                break;
+        }
+    
+        return `
+                <div>
+                    <div class="log_profile border-default surface-alt">
+                    </div>
+                </div>
+                <div>
+                    <p class="text-default display-medium14">나</p>
+                    <p class="text-default display-medium14">${template}</p>
+                    <p class="text-weak display-medium12">${time}${timeType} 전</p>
+                </div>
+        `;
+    }
+
+    function templateFooter() {
+        return `
+                <button class="display-bold14 text-danger">기록 전체 삭제</button>
+            `;
+    }
+
+    function render(logs) {
+        const logElement = document.createElement('div');
+        logElement.setAttribute('id', 'log_layer');
+        logElement.setAttribute('class', 'surface-default rounded-200 shadow-floating');
+        logElement.innerHTML = template();
+
+        const logListElement = logElement.querySelector('#log_layer_list');
+
+        if(!logs || logs.length === 0)   {
+            const emptyLogElement = renderEmpty();
+            logListElement.appendChild(emptyLogElement);
+        }
+        else {
+            const logFragElement = document.createDocumentFragment();
+            logs.forEach(el => {
+                const logLiElement = renderLog(el);
+                logFragElement.appendChild(logLiElement);
+            })
+            logListElement.append(logFragElement);
+
+            const logFooterElement = document.createElement('div');
+            logFooterElement.innerHTML = templateFooter();
+            logFooterElement.setAttribute('id', 'log_layer_footer')
+            logElement.appendChild(logFooterElement);
+        }  
+
+        return logElement;
+    }
+
+    function remove() {
+        const logElement = document.body.querySelector('#log_layer');
+        if(logElement) {
+            document.body.removeChild(logElement);
+        }
+    }
+
+    function renderEmpty() {
+        const logElement = document.createElement('li');
+        logElement.setAttribute('id', 'log_layer_empty');
+        logElement.innerHTML = templateEmpty();
+
+        return logElement
+    }
+
+    function renderLog(log) {
+        const logLiElement = document.createElement('li');
+        logLiElement.innerHTML = templateLog(log);
+
+        return logLiElement;
+    }
+
+    function addEventListener(logElement, handleClose, handleClear) {
+        const [closeButton, clearButton] = logElement.getElementsByTagName('button');
+
+        closeButton.addEventListener('click', handleClose);
+
+        if(clearButton) {
+            clearButton.addEventListener('click', handleClear)
+        }
+    }
+
+    function findColumnName(columnIdx) {
+        return columns.find(el => el.index === columnIdx).title;
+    }
+
+    return {
+        render,
+        remove,
+        addEventListener,
+    }
 }
--- script/state.js
+++ script/state.js
@@ -1,26 +1,42 @@
 export default function State() {
-    let orderingState = -1;
-    
+    let orderingState = 1;
+    let taskId = 0;
+
     let columnTasks = [];
     let columns = [
         {
-            title: '해야할 일',
+            title: "해야할 일",
             index: 0,
         },
         {
-            title: '하고 있는 일',
+            title: "하고 있는 일",
             index: 1,
         },
         {
-            title: '완료한 일',
+            title: "완료한 일",
             index: 2,
-        }
+        },
     ];
+    let logs = [];
 
     let dragged = {
         task: null,
         element: null,
     };
+
+    function setLog(log) {
+        if (logs.length >= 5) {
+            logs = [...logs.slice(1), log];
+        } else logs.push(log);
+    }
+
+    function getLog() {
+        return logs;
+    }
+
+    function clearLog() {
+        logs = [];
+    }
 
     function getOrder() {
         return orderingState;
@@ -28,6 +44,7 @@ export default function State() {
 
     function flipOrder() {
         orderingState = -orderingState;
+        return orderingState;
     }
 
     function init() {
@@ -51,25 +68,52 @@ export default function State() {
         dragged = {
             task: null,
             element: null,
-        }
+        };
     }
 
     function getColumns() {
-        return {columns, columnTasks};
+        return { columns, columnTasks };
     }
 
     function addTask(index, task) {
-        columnTasks[index].push(task);
+        const newId = taskId++;
+        const newTask = {
+            ...task,
+            taskId: newId,
+        };
+        setLog({
+            task: newTask,
+            type: "ADD",
+            updated: new Date(),
+        });
+        columnTasks[index].push(newTask);
+
+        return newId;
     }
 
     function updateTask(index, currentTask, newTask) {
+        setLog({
+            task: currentTask,
+            type: "UPDATE",
+            updated: new Date(),
+        });
+
         const taskIndex = columnTasks[index].indexOf(currentTask);
-        columnTasks[index][taskIndex] = newTask;
+        columnTasks[index][taskIndex] = {...newTask, taskId: currentTask.taskId};
     }
 
     function removeTask(task) {
+        setLog({
+            task: task,
+            type: "REMOVE",
+            updated: new Date(),
+        });
         const index = task.column;
-        columnTasks[index] = columnTasks[index].filter(el => el != task);
+        columnTasks[index] = columnTasks[index].filter((el) => el.taskId != task.taskId);
+    }
+
+    function sortTask(task) {
+        return task.sort((a, b) => orderingState * (a.created - b.created))
     }
 
     init();
@@ -84,5 +128,8 @@ export default function State() {
         addTask,
         updateTask,
         removeTask,
-    }
+        getLog,
+        clearLog,
+        sortTask, 
+    };
 }
