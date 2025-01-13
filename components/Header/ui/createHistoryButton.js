4946d69ca5751acb5082abda75034d38eeb90145
rbgksqkr <rbgks1937@naver.com>
2025-01-13 18:36:21 +0900

    feat: history 버튼 클릭 시 사용자 활동 기록 창 애니메이션 적용

--- components/Header/ui/createHistoryButton.js
+++ components/Header/ui/createHistoryButton.js
@@ -1,8 +1,13 @@
 import { IMAGE } from "../../../assets/index.js";
-import { createElement, createImg } from "../../../dom.js";
+import { createButton, createImg } from "../../../dom.js";
 
 const createHistoryButton = () => {
-  const $historyButton = createElement("button");
+  const handleClickHistory = (e) => {
+    const $history = e.currentTarget.nextSibling;
+    $history.classList.toggle("fade-in");
+  };
+
+  const $historyButton = createButton({ handleClick: handleClickHistory });
   const $img = createImg({
     src: IMAGE.clock,
     alt: "사용자 활동 기록",
