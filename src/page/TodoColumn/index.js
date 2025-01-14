9689edc993186cb484d804b99139dd5553210ba0
hamo-o <yheel369@gmail.com>
2025-01-14 14:41:43 +0900

    feat: 컬럼 UI 마크업, 투두 id 기반 생성 및 삭제 구현

--- src/page/TodoColumn/index.js
+++ src/page/TodoColumn/index.js
@@ -3,18 +3,30 @@ import { ColumnTitle } from "../../components/ColumnTitle/index.js";
 import { useState } from "../../lib/hooks/useState.js";
 import { parser } from "../../lib/jsx-runtime/index.js";
 
+import styles from "./todoColumn.module.js";
+
 export const TodoColumn = ({ title }) => {
   const [todos, setTodos] = useState([]);
 
   const handleClickAddTodo = () => {
-    const newTodo = {};
+    const newTodo = {
+      id: new Date().getTime(),
+    };
     setTodos([...todos, newTodo]);
   };
 
+  const handleClickDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return parser`
-        <div>
+        <div class="${styles.container}">
             ${ColumnTitle({ title, count: todos.length, onClickPlus: handleClickAddTodo })}
-            <ul>
+            <ul class="${styles.list}">
+                ${todos.map((todo) => parser`
+                  <li key="${todo.id}">
+                    ${Card({ type: "add-edit", onClickDelBtn: () => handleClickDeleteTodo(todo.id) })}
+                  </li>`)}
             </ul>
         </div>
     `;
