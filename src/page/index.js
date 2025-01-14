135680686a14fd9b101bb3c7d5615f802cb117b1
hamo-o <yheel369@gmail.com>
2025-01-14 15:10:22 +0900

    feat: 컬럼 추가

--- src/page/index.js
+++ src/page/index.js
@@ -1,3 +1,5 @@
+import { FAB } from "../components/FAB/index.js";
+import { useState } from "../lib/hooks/useState.js";
 import { parser } from "../lib/jsx-runtime/parser.js";
 
 import { Header } from "./Header/index.js";
@@ -5,13 +7,20 @@ import styles from "./page.module.js";
 import { TodoColumn } from "./TodoColumn/index.js";
 
 const MainPage = () => {
-  const columns = [{ title: "해야할 일" }, { title: "하고 있는 일" }, { title: "완료한 일" }];
+  const [columns, setColumns] = useState([{ title: "해야할 일" }, { title: "하고 있는 일" }, { title: "완료한 일" }]);
+  const handleClickAddColumn = () => {
+    const newColumn = {
+      title: "새로운 컬럼",
+    };
+    setColumns([...columns, newColumn]);
+  };
   return parser`
     <div>
         ${Header()}
         <main class="${styles.container}">
             ${columns.map(({ title }) => TodoColumn({ title }))}
         </main>
+        ${FAB({ onClick: handleClickAddColumn })}
     </div>
 `;
 };
