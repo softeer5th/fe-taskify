7cf1f9d6b0d04abd3813abb681b924293b76d32c
pepperdad <becomegoodguy@gmail.com>
2025-01-14 18:48:53 +0900

    feat: 아이템 렌더 시, localStroage 스펙 별 불러오는 로직 추가

--- js/blocks/Header/index.js
+++ js/blocks/Header/index.js
@@ -1,3 +1,10 @@
+// TODO: 파일 따로 분리
+const CATEGORY = {
+    todo: '해야할 일',
+    doing: '하고 있는 일',
+    done: '완료한 일',
+};
+
 export const toggleClockBtn = () => {
     const clockBtn = document.querySelector('#clock-btn');
     const activityList = document.querySelector('.activity__list');
@@ -9,19 +16,57 @@ export const toggleClockBtn = () => {
 export const renderRecords = () => {
     const recordsFromStorage = JSON.parse(localStorage.getItem('records')) || [];
 
+    if(recordsFromStorage.length === 0) {
+        activityList.appendChild(document.createElement('li')).textContent = '사용자 활동 기록이 없습니다.';
+        return ;
+    }
+    
+    const activityContianer = document.createElement('ul');
     const activityList = document.querySelector('.activity__list');
     recordsFromStorage.forEach((record, index) => {
-        const taskItem = document.createElement('li');
-        taskItem.className = 'task-item';
+        const recordContainer = document.createElement('li');
+        recordContainer.className = 'record__container';
+        
+        const textBox = document.createElement('div');
+        textBox.className = 'text__box';
 
-        const taskContent = document.createElement('div');
-        taskContent.textContent = `${record.title} (${record.beforeCategory} → ${record.afterCategory})`;
+        const userImg = document.createElement('img');
+        userImg.src = '/assets/user.png';
+        const username = document.createElement('p');
+        username.textContent = '@이름이용';
+        const content = document.createElement('p');
+        let type;
+        switch (record.type) {
+            case 'add':
+                type = '추가';
+                break;
+            case 'move':
+                type = '이동';
+                break;
+            case 'update':
+                type = '변경';
+                break;
+            case 'register':
+                type = '등록';
+                break;
+            case 'delete':
+                type = '삭제';
+                break;
+            default:
+                type = '알 수 없음';
+                break;
+        }
+        content.innerHTML = `<strong>${record.title}</strong>을(를) <strong>${CATEGORY[record.beforeCategory]}</strong>에서 <strong>${CATEGORY[record.afterCategory]}</strong>으로 <strong>${type}</strong> 하였습니다.`;
+        const time = document.createElement('p');
+        time.textContent = `3분전`;
 
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-
-        taskItem.appendChild(taskContent);
-        taskItem.appendChild(deleteButton);
-        activityList.appendChild(taskItem);
+        textBox.appendChild(username);
+        textBox.appendChild(content);
+        textBox.appendChild(time);
+        recordContainer.appendChild(userImg);
+        recordContainer.appendChild(textBox);
+        activityContianer.appendChild(recordContainer);
     });
+
+    activityList.appendChild(activityContianer);
 }
