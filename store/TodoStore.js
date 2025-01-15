7c4b799a99d9c49c82d615206f9d7c7d4d7c1e84
rbgksqkr <rbgks1937@naver.com>
2025-01-15 17:36:39 +0900

    feat: TodoStore 구현

---
+++ store/TodoStore.js
@@ -0,0 +1,46 @@
+import { STORAGE_KEY } from "../constants/storageKey.js";
+import {
+  clearLocalStorage,
+  loadLocalStorage,
+  saveLocalStorage,
+} from "../utils/localStorage.js";
+import { getRandomId } from "../utils/random.js";
+import Observable from "./Observable.js";
+
+class TodoStore extends Observable {
+  #todoList = loadLocalStorage(STORAGE_KEY.todoList) || [];
+
+  add({ sectionId, todo }) {
+    const newTodo = {
+      ...todo,
+      id: getRandomId(),
+      createdAt: new Date(),
+    };
+
+    this.#todoList = [...this.#todoList].map((section) =>
+      section.id === sectionId
+        ? { ...section, items: [...section.items, newTodo] }
+        : section
+    );
+    this.notify({ sectionId, newTodo, todoList: this.#todoList });
+    saveLocalStorage(STORAGE_KEY.todoList, this.#todoList);
+  }
+
+  clear() {
+    this.#todoList = [];
+    this.notify(this.#todoList);
+    clearLocalStorage(STORAGE_KEY.todoList);
+  }
+
+  get todoList() {
+    return this.#todoList;
+  }
+
+  set todoList(data) {
+    this.#todoList = data;
+  }
+}
+
+const todoStore = new TodoStore();
+
+export default todoStore;
