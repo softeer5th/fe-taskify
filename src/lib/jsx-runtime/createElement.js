b32436aad03f35367bee26f751905588c7df94eb
hamo-o <yheel369@gmail.com>
2025-01-15 18:28:29 +0900

    fix: 삭제했던 jsDoc 복구

--- src/lib/jsx-runtime/createElement.js
+++ src/lib/jsx-runtime/createElement.js
@@ -1,3 +1,23 @@
+/**
+ * @typedef VNode
+ * @type {string | number | VDOM | null | undefined}
+ */
+
+/**
+ * @typedef VDOM
+ * @type {object}
+ * @property {string} tag - 생성할 HTML 태그의 종류.
+ * @property {{ [key: string]: unknown } | null } props - 생성할 HTML 태그의 속성 객체.
+ * @property {VNode[]} children - 생성할 HTML 태그의 자식 요소.
+ */
+
+/**
+ *
+ * @param {string} type - 생성할 가상 DOM의 HTML 태그 이름
+ * @param {{ [key: string]: unknown } | null } config - 생성할 HTML 태그의 속성 객체
+ * @param {VNode[]} children - 생성할 HTML 태그의 자식 요소
+ * @returns {VDOM} - 생성된 가상 DOM
+ */
 export const createElement = (type, config, ...children) => {
   const formatConfigs = () => {
     const props = {};
