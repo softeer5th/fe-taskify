5211e8784d29f806bf4443176ef3ea89383a405c
kimdongyoung <wwweric12@naver.com>
2025-01-16 16:24:48 +0900

    fix: chip 컴포넌트 수정

--- src/components/Chip/Chip.js
+++ src/components/Chip/Chip.js
@@ -1,15 +1,15 @@
 import { getIconSVG } from '../../utils/iconUtils.js';
 import { loadCss } from '../../utils/loadcss.js';
 
-export function Chip(iconType,iconColor,content,eventFuntion){
+export function Chip({iconType,iconColor,content,id}){
     const chip = document.createElement('button');
     chip.className = "chip-container";
+    chip.id= id;
     chip.innerHTML = getIconSVG(iconType,iconColor)
     chip.insertAdjacentHTML('beforeend', `
         <div class="chip-content">${content}</div>
     `);
     
-    chip.addEventListener('click', eventFuntion);
 
     loadCss('../src/components/chip/chip.css')
     return chip;
