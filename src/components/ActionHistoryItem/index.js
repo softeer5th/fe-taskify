39704d565f3b0ab9bd53795011be78b25d0115c6
hamo-o <yheel369@gmail.com>
2025-01-16 11:39:41 +0900

    feat: jsDoc 타입 추가, N분 전을 알리는 유틸함수 구현

--- src/components/ActionHistoryItem/index.js
+++ src/components/ActionHistoryItem/index.js
@@ -1,18 +1,19 @@
 import { typos } from "../../constants/tokens/typos.js";
 import { parser } from "../../lib/jsx-runtime/index.js";
+import { calcBeforeTime } from "../../utils/date.js";
 
 import styles from "./actionHistoryItem.module.js";
 
 /**
  *
- * @param props
- * @param props.action
- * @param props.title
- * @param props.columnBefore
- * @param props.columnAfter
- * @param props.username
- * @param props.timestamp
- * @returns {VDOM} -
+ * @param {object} props - 액션 히스토리 아이템 컴포넌트에 전달되는 props.
+ * @param {"등록"|"삭제"|"변경"|"이동"} props.action - 액션 타입.
+ * @param {string} props.title - 액션의 대상 투두의 제목.
+ * @param {string} [props.columnBefore] - 액션 이전 컬럼 이름.
+ * @param {string} [props.columnAfter] - 액션 이후 컬럼 이름.
+ * @param {string} props.username - 액션을 수행한 사용자 이름.
+ * @param {Date} props.timestamp - 액션 수행 시간.
+ * @returns {VDOM} - 액션 히스토리 아이템 컴포넌트.
  */
 export const ActionHistoryItem = ({
   username, title, columnBefore, columnAfter, action, timestamp,
@@ -35,7 +36,7 @@ export const ActionHistoryItem = ({
                 ${formatTextToBoldTag(columnAfter)}으로 
                 ${formatTextToBoldTag(action)}하였습니다.
             </div>
-            <div class="${typos.display.medium[12]} ${styles.timestamp}">${timestamp}</div>
+            <div class="${typos.display.medium[12]} ${styles.timestamp}">${calcBeforeTime(timestamp)}</div>
         </div>
     </div>
     `;
